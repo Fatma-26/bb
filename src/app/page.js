cb79a7edddeea71ce68f25e393aac1e2a8074675
Fatma-26 <yasmeenyahya26@gmail.com>
2024-09-17 14:32:58 +0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import Head from "next/head";
 import Section1 from "../../components/section1";
+import Section2 from "../../components/section2";
+import Section3 from "../../components/section3";
 import Format from "../../layout/format";
 export default function Home() {
   return (
    
     <Format>
       <Section1></Section1>
-
+      <Section2></Section2>
+      <Section3></Section3>
     </Format>
     
   );
