9a70176fef24239f8ca510da916faa63e1357111
Fatma-26 <yasmeenyahya26@gmail.com>
2024-09-18 14:34:28 +0300

    Update header.js

--- components/header.js
+++ components/header.js
@@ -9,7 +9,7 @@ export default function header () {
              <input type='text'  className="focus:outline-none focus:border-sky-500 focus:ring-1 focuse;ring-sky-500  mt-1block w-60 px-3 py-2 bg-white border border-slate-300 rounded-full text-sm shadow-sm placeholder-slate-500"  placeholder="Search..."/> 
         </div>
          <div className="shrink w-80 sm:order-2" >
-            <Link href={"/"}> <a className="font-bold uppercase text-3xl">Design</a> </Link>
+            <Link href={"/"}> <a className="font-bold uppercase text-3xl text-black">Design</a> </Link>
             
          </div>
           <div className=" w-96 order-3 flex justify-center "> 
